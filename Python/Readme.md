78721968cc27593fc372b6d4823e292af3d2f73d
Leon L. Robinson <leon.robinson@qa.com>
2023-07-18 12:10:26 +0100

    Collections

---
+++ Python/Readme.md
@@ -0,0 +1,25 @@
+# Python
+
+  01010111001
+
+- String
+- Booleam
+- Integer
+- Float
+
+## Errors, Syntax vs Logic
+
+prroint('Hello World)
+
+# Maths symbols
+
++ -  / * ** % 
+
+# Conversion functions
+str()
+int()
+bool()
+float()
+
+# reading input
+input()
---
+++ Python/collections.py
@@ -0,0 +1,41 @@
+# Python List
+
+listvar1 = ["Meat", "Vegetables", "Cake", "Beer for the weekend"]
+
+print(listvar1)
+
+print(listvar1[0])
+
+listvar2 = 
+
+cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"] 
+cool_pigs.append("Hamton")
+cool_pigs.remove("Hogwarts")
+cool_pigs.insert(1,"Babe")
+cool_pigs.count('Babe')
+len(cool_pigs)
+
+# tuple
+
+tuple_cool_pigs = ("Chris P. Bacon", "Hamlet", "Hogwarts")
+
+
+# Lists & String conversion
+
+joinvar=" and "                                                                 
+listvar = ['Meat', 'Vegetables', 'Cake', 'Beer for the weekend']
+joinvar.join(listvar)
+
+listvar2 = "Meat,Vegetables,Cake,Beer for the weekend"
+listvar2.split(',')
+
+
+
+
+# Dictionary
+
+dictionary_variable = {'name': 'Leon', 'cool': True, 'Watch': 'Charged', 'Awesomeness': 9001}
+
+print(dictionary_variable['cool'])
+dictionary_variable["screens"] = 4 
+dictionary_variable["Awesomeness"] = 200000000
